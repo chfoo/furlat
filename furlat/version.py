be53447b17951b16403fd823d3d95dc4ad1134f9
Christopher Foo <chris.foo@gmail.com>
2013-07-28 03:38:38 -0400

    Bumps version to 0.1

--- furlat/version.py
+++ furlat/version.py
@@ -2,6 +2,6 @@
 # Copyright 2013 Christopher Foo <chris.foo@gmail.com>
 # Licensed under GNU GPL v3. See COPYING.txt for details
 
-__version__ = '0.0'
+__version__ = '0.1'
 
 __all__ = ('__version__',)
